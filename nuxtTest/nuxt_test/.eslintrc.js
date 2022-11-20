05b2e00b58340d232734b7e6df3335039257e591
doodoodoong <doo_doong@hanmail.net>
2022-11-21 01:13:02 +0900

    nuxtjs+vuetify+tailwindcss

--- nuxtTest/nuxt_test/.eslintrc.js
+++ nuxtTest/nuxt_test/.eslintrc.js
@@ -2,18 +2,18 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
   },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
+  extends: ["@nuxtjs", "plugin:nuxt/recommended"],
+  plugins: [],
   // add your custom rules here
-  rules: {}
-}
+  rules: {
+    "space-before-blocks": "off",
+    quotes: "off",
+    "no-extra-semi": "off",
+  },
+};
---
+++ nuxtTest/nuxt_test/pages/test.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <p class="text-red-300 text-3xl underline">Hello world!</p>
+  </div>
+</template>
+<script>
+export default {};
+</script>
